ff4f6898093afddd4fbdf2b8ce1822faf96ebb80
Shahid Noor <snoor1@lenovo.com>
2025-08-26 11:22:15 +0530

    PageProps

--- src/app/categories/[slug]/page.tsx
+++ src/app/categories/[slug]/page.tsx
@@ -1,13 +1,17 @@
-interface CategoryPageProps {
+/* interface CategoryPageProps {
   params: { slug: string };
 }
+ */
 
-export default async function CategoryPage({ params }: CategoryPageProps) {
-const { slug } = params;
+type Params = Promise<{ slug: string }>
+export default async function CategoryPage( props: { params: Params }) {
+  const params = await props.params;
+  const quiz = params.slug;
+
   return (
     <div>
-      <h1 className="text-2xl font-bold capitalize">{slug} Quiz</h1>
-      <p className="mt-2 text-gray-600">Here will be the quizzes for {slug}.</p>
+      <h1 className="text-2xl font-bold capitalize">{quiz} Quiz</h1>
+      <p className="mt-2 text-gray-600">Here will be the quizzes for {quiz}.</p>
     </div>
   );
 }
